38e3f1226bf9e097d9400efb6cf4a291e65636a7
Upscale2765 <antieverything160@gmail.com>
2024-05-16 13:42:04 -0400

    Defined quarter_round function

--- chacha20.cpp
+++ chacha20.cpp
@@ -1,6 +1,10 @@
 #include <cstdint>
 #include "chacha20.h"
 
+void ChaCha20::quarter_round(uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d) {
+
+}
+
 ChaCha20::ChaCha20(uint32_t *key, uint32_t *nonce) {
 	this -> chacha_state = new uint32_t[16] {
         0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,
