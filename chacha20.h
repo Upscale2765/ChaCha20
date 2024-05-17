eee6062ca07f249e1320b6a62d17c691d732f725
Upscale2765 <antieverything160@gmail.com>
2024-05-17 10:58:25 -0400

    Made quarter round and left roll private

--- chacha20.h
+++ chacha20.h
@@ -3,9 +3,9 @@
 class ChaCha20 {
 	private:
 		uint32_t *chacha_state;
+		uint32_t l_roll(uint32_t w, unsigned int b);
+		void quarter_round(uint32_t *state, unsigned int a, unsigned int b, unsigned int c, unsigned int d);
 
 	public:
 		ChaCha20(uint32_t *key, uint32_t *nonce);
-		uint32_t l_roll(uint32_t w, unsigned int b);
-		void quarter_round(uint32_t *state, unsigned int a, unsigned int b, unsigned int c, unsigned int d);
 };
