4d514e8c092b0565de41a17bb3460aeb5e8b3045
Upscale2765 <antieverything160@gmail.com>
2024-05-17 11:15:26 -0400

    Read key and nonce as little endian

--- chacha20.cpp
+++ chacha20.cpp
@@ -33,7 +33,8 @@ void ChaCha20::quarter_round(uint32_t *s, unsigned int a, unsigned int b, unsign
 ChaCha20::ChaCha20(uint32_t *key, uint32_t *nonce) {
 	chacha_state = new uint32_t[16] {
         0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,
-        key[0], key[1], key[2], key[3], key[4], key[5], key[6], key[7],
-        0x00000000, nonce[0], nonce[1], nonce[2], 
+        e_flip(key[0]), e_flip(key[1]), e_flip(key[2]), e_flip(key[3]),
+        e_flip(key[4]), e_flip(key[5]), e_flip(key[6]), e_flip(key[7]),
+        0x00000000, e_flip(nonce[0]), e_flip(nonce[1]), e_flip(nonce[2]), 
     };
 }
