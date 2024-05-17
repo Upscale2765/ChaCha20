20626246a67ecad3da751a99067589fa83585beb
Upscale2765 <antieverything160@gmail.com>
2024-05-17 11:00:14 -0400

    Declared e_flip for flipping uint32_t endian

--- chacha20.h
+++ chacha20.h
@@ -4,6 +4,7 @@ class ChaCha20 {
 	private:
 		uint32_t *chacha_state;
 		uint32_t l_roll(uint32_t w, unsigned int b);
+		uint32_t e_flip(uint32_t w);
 		void quarter_round(uint32_t *state, unsigned int a, unsigned int b, unsigned int c, unsigned int d);
 
 	public:
