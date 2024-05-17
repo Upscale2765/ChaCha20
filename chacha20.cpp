94e594909b79167cbc4e5b4c0742063ebcbd3046
Upscale2765 <antieverything160@gmail.com>
2024-05-17 11:13:34 -0400

    Implemented e_flip

--- chacha20.cpp
+++ chacha20.cpp
@@ -1,11 +1,18 @@
 #include <cstdint>
 #include "chacha20.h"
 
+uint32_t ChaCha20::e_flip(uint32_t w) {
+    return (w << 24) |
+          ((w <<  8) & 0x00ff0000) |
+          ((w >>  8) & 0x0000ff00) |
+          ((w >> 24) & 0x000000ff);
+}
+
 uint32_t ChaCha20::l_roll(uint32_t w, unsigned int b) {
     return (w << b) | (w >> (32 - b));
 }
 
-void ChaCha20::quarter_round(uint32_t *s, uint32_t a, uint32_t b, uint32_t c, uint32_t d) {
+void ChaCha20::quarter_round(uint32_t *s, unsigned int a, unsigned int b, unsigned int c, unsigned int d) {
     s[a] += s[b];
     s[d] ^= s[a];
     s[d] = l_roll(s[d], 16);
